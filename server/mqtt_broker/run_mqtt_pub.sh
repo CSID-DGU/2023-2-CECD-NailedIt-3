5938577e357254e29410e50dae2a93de4e3334be
jun6292 <c68254@gmail.com>
2024-04-30 22:43:21 +0900

    fix: 파일비츠 output 버그 수정

--- server/mqtt_broker/run_mqtt_pub.sh
+++ server/mqtt_broker/run_mqtt_pub.sh
@@ -3,7 +3,7 @@
 nohup sh -c '
     while true
     do
-        mosquitto_pub -t log -m "timestamp: 2024-03-19 12:42:23, id: 3, pH: 7.12, DO(PPM): 8.71, salinity(%): 0.36, turbidity(NTU): 7.59, ammonia(mg/L): 0.01"
+        mosquitto_pub -t log -m "timestamp: 2024-04-30 12:42:23, id: 1, pH: 7.12, DO(PPM): 8.71, salinity(%): 0.36, turbidity(NTU): 7.59, ammonia(mg/L): 0.01"
         sleep 5
     done' > /dev/null 2>&1 &
 
