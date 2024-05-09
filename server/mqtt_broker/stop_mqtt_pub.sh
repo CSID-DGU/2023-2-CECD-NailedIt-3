4e9ab10809b9d31eb23275dd6d353289beda928b
Kang Woobin <woobin929@dgu.ac.kr>
2024-05-09 19:42:21 +0900

    mqtt 스크립트 작성

--- server/mqtt_broker/stop_mqtt_pub.sh
+++ server/mqtt_broker/stop_mqtt_pub.sh
@@ -10,11 +10,10 @@
 
 #!/bin/sh
 # stop_mqtt_pub.sh
-if [ ! -f script.pid ]; then
-    echo "PID file not found."
+PID=$(pgrep -f script.py)
+if [ -z "$PID" ]; then
+    echo "No process found."
 else
-    PID=$(cat script.pid)
     kill $PID
-    rm script.pid
     echo "Process with PID $PID has been stopped."
 fi
