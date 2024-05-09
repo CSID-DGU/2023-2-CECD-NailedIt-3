c908b575ac80ceca139ec52d0ad8f49fbd45e1b5
Kang Woobin <woobin929@dgu.ac.kr>
2024-05-09 19:42:21 +0900

    mqtt 스크립트 작성

--- server/mqtt_broker/run_mqtt_pub.sh
+++ server/mqtt_broker/run_mqtt_pub.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-nohup sh -c '
-    while true
-    do
-        mosquitto_pub -t log -m "timestamp: 2024-04-30 12:42:23, id: 1, pH: 7.12, DO(PPM): 8.71, salinity(%): 0.36, turbidity(NTU): 7.59, ammonia(mg/L): 0.01"
-        sleep 5
-    done' > /dev/null 2>&1 &
+nohup python script.py > /dev/null 2>&1 &
 
-echo $! > script.pid
 echo "Script is running in background."
--- server/mqtt_broker/script.py
+++ server/mqtt_broker/script.py
@@ -1,34 +1,55 @@
 import paho.mqtt.publish as publish
-import json
 import random
 import time
-from datetime import datetime
 
-def publish_sensor_data():
-    # 현재 시각을 timestamp로 사용
-    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    # 랜덤 값 생성
-    ph = round(random.uniform(6.5, 8.5), 2) # pH는 6.5에서 8.5 사이
-    dissolved_oxygen = round(random.uniform(5.0, 12.0), 2) # 용존 산소량은 5.0에서 12.0 PPM 사이
-    salinity = round(random.uniform(0.0, 5.0), 2) # 염분은 0.0%에서 5.0% 사이
-    turbidity = round(random.uniform(0.0, 30.0), 2) # 탁도는 0.0에서 30.0 NTU 사이
-    ammonia = round(random.uniform(0.0, 1.0), 2) # 암모니아는 0.0에서 1.0 mg/L 사이
-    # 메시지 구성
-    message = json.dumps({
-        "timestamp": timestamp,
-        "pH": ph,
-        "DO(PPM)": dissolved_oxygen,
-        "salinity(%)": salinity,
-        "turbidity(NTU)": turbidity,
-        "ammonia(mg/L)": ammonia
-    })
-    # message = f"timestamp: {timestamp}, pH: {ph}, DO(PPM): {dissolved_oxygen}, salinity(%): {salinity}, turbidity(NTU): {turbidity}, ammonia(mg/L): {ammonia}"
+# MQTT 브로커 정보 설정
+mqtt_broker = "your_mqtt_broker_address"
+mqtt_port = 1883
+mqtt_topic = "log"
+
+# 무작위 값 생성 함수
+def generate_random_data():
+    timestamp = time.strftime("%Y-%m-%d %H:%M:%S")
+    #id = random.randint(1, 3)
+    pH = round(random.uniform(6.5, 8.5), 2)
+    dissolved_oxygen = round(random.uniform(7.0, 9.0), 2)
+    salinity = round(random.uniform(0.3, 0.4), 2)
+    turbidity = round(random.uniform(5.0, 10.0), 2)
+    ammonia = round(random.uniform(0.0, 0.1), 2)
     
-    # Mosquitto 브로커로 메시지 발행
-    publish.single("sensor/log", message, hostname="mosquitto", client_id="filebeat_mqtt", protocol=publish.MQTTv311)
+    pH2 = round(random.uniform(6.5, 8.5), 2)
+    dissolved_oxygen2 = round(random.uniform(7.0, 9.0), 2)
+    salinity2 = round(random.uniform(0.3, 0.4), 2)
+    turbidity2 = round(random.uniform(5.0, 10.0), 2)
+    ammonia2 = round(random.uniform(0.0, 0.1), 2)
+    
+    pH3 = round(random.uniform(6.5, 8.5), 2)
+    dissolved_oxygen3 = round(random.uniform(7.0, 9.0), 2)
+    salinity3 = round(random.uniform(0.3, 0.4), 2)
+    turbidity3 = round(random.uniform(5.0, 10.0), 2)
+    ammonia3 = round(random.uniform(0.0, 0.1), 2)
 
-# 주기적으로 MQTT 메시지 발행
-while True:
-    publish_sensor_data()
-    print("Data published.")
-    time.sleep(10) # 10초 간격으로 데이터 발행
+    message = f"timestamp: {timestamp}, id: 1, pH: {pH}, DO(PPM): {dissolved_oxygen}, salinity(%): {salinity}, turbidity(NTU): {turbidity}, ammonia(mg/L): {ammonia}"
+    message2 = f"timestamp: {timestamp}, id: 2, pH: {pH2}, DO(PPM): {dissolved_oxygen2}, salinity(%): {salinity2}, turbidity(NTU): {turbidity2}, ammonia(mg/L): {ammonia2}"
+    message3 = f"timestamp: {timestamp}, id: 3, pH: {pH3}, DO(PPM): {dissolved_oxygen3}, salinity(%): {salinity3}, turbidity(NTU): {turbidity3}, ammonia(mg/L): {ammonia3}"
+    publish_mqtt_message(message)
+    publish_mqtt_message(message2)
+    publish_mqtt_message(message3)
+    print("Published:", message)
+    print("Published2:", message2)
+    print("Published3:", message3)
+    
+
+# MQTT 메시지 발행 함수
+def publish_mqtt_message(message):
+    publish.single(mqtt_topic, message, port=mqtt_port)
+
+# 메인 루프
+if __name__ == "__main__":
+    try:
+        while True:
+            generate_random_data()
+            
+            time.sleep(10)  # 5초마다 메시지 전송
+    except KeyboardInterrupt:
+        print("Exiting...")
