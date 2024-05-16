f63f38a7f5fed315c0081dbbf5cb9b3cb8ce1c39
jun6292 <c68254@gmail.com>
2024-05-16 20:08:53 +0900

    Comment: 주석 삭제

--- server/mqtt_broker/stop_mqtt_pub.sh
+++ server/mqtt_broker/stop_mqtt_pub.sh
@@ -1,15 +1,5 @@
 # #!/bin/sh
 
-# PID=$(pgrep -f script.py)
-# if [ -z "$PID" ]; then
-#     echo "No process found."
-# else
-#     kill $PID
-#     echo "Process with PID $PID has been stopped."
-# fi
-
-#!/bin/sh
-# stop_mqtt_pub.sh
 PID=$(pgrep -f script.py)
 if [ -z "$PID" ]; then
     echo "No process found."
